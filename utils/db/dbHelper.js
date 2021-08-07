a4e4732c334a773fec8dcfa83c50d0052e6ba9be
Aamir Khan <aamirkhan8878@gmail.com>
2021-08-07 12:35:43 +0530

    Update

--- utils/db/dbHelper.js
+++ utils/db/dbHelper.js
@@ -1,13 +1,20 @@
 const db = require("../../config/db");
 
 const dbexe = async (query, arr = [], records = "all") => {
-  let data;
+  let data = false;
   if (arr.length > 0) {
     const [rows] = await db.query(query, arr);
-    data = rows.length == 1 && records == "single" ? rows[0] : rows;
+    if (rows.length > 0) {
+      data = rows.length == 1 && records == "single" ? rows[0] : rows;
+    }
   } else {
     const [rows] = await db.query(query);
-    data = rows.length == 1 && records == "single" ? rows[0] : rows;
+    if (rows.length > 0) {
+      data = rows.length == 1 && records == "single" ? rows[0] : rows;
+    }
   }
+  console.log(data);
   return data;
 };
+
+module.exports = { dbexe, db };
